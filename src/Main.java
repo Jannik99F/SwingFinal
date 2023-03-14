78029a0eaa9516d823650d779560e12ba3e5c246
Mendax1 <abaris004@gmail.com>
2023-03-14 14:13:35 +0100

    Design zu Nimbus geändert, Runnable von intellij durch lambda ersetzt

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
+import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Start start = new Start( 1);
-                    start.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
+
+        //Code um Nimbus Design zu verwenden
+        //source: https://stackoverflow.com/questions/4617615/how-to-set-nimbus-look-and-feel-in-main
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
                 }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, fall back to cross-platform
+            try {
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("An error has occurred, program exit.");
+            }
+        }
+        EventQueue.invokeLater(() -> {
+            try {
+                Start start = new Start( 1);//Variable damit festgestellt werden kann ob es das erste Spiel ist
+                start.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
